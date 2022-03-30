6363273e8094bd3851a79f8e83d2193707240a42
Artur Androsovych <arthurandrosovich@gmail.com>
2022-03-30 08:10:46 +0300

    Initial setup (#3)

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,17 @@
-describe("@esm-bundle/autopublish-template", () => {
-  it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
-  });
+describe("@esm-bundle/angular__animations", () => {
+  ["es2015", "es2020"].forEach((ecma) => {
+    it(`can load the System.register ${ecma} bundle`, async () => {
+      const m = await System.import(
+        `/base/system/${ecma}/ivy/angular-animations.js`
+      );
+      expect(m.AnimationBuilder).toBeDefined();
+    });
 
-  it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+    it(`can load the System.register ${ecma} prod bundle`, async () => {
+      const m = await System.import(
+        `/base/system/${ecma}/ivy/angular-animations.min.js`
+      );
+      expect(m.AnimationBuilder).toBeDefined();
+    });
   });
 });
