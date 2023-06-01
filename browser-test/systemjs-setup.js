e1c7a489884db88988e83f85c00df2383cc87afc
arturovt <arthurandrosovich@gmail.com>
2023-06-01 08:25:24 +0300

    Upgrade to Angular 16

--- browser-test/systemjs-setup.js
+++ browser-test/systemjs-setup.js
@@ -6,9 +6,9 @@ document.head.appendChild(
         "imports": {
           "rxjs": "https://cdn.jsdelivr.net/npm/@esm-bundle/rxjs/system/es2015/rxjs.min.js",
           "rxjs/operators": "https://cdn.jsdelivr.net/npm/@esm-bundle/rxjs/system/es2015/rxjs-operators.min.js",
-          "@angular/core": "https://cdn.jsdelivr.net/npm/@esm-bundle/angular__core/system/es2015/ivy/angular-core.js",
-          "@angular/common": "https://cdn.jsdelivr.net/npm/@esm-bundle/angular__common/system/es2015/ivy/angular-common.js",
-          "@angular/animations": "/base/system/es2015/ivy/angular-animations.min.js"
+          "@angular/core": "https://cdn.jsdelivr.net/npm/@esm-bundle/angular__core/system/es2022/ivy/angular-core.js",
+          "@angular/common": "https://cdn.jsdelivr.net/npm/@esm-bundle/angular__common/system/es2022/ivy/angular-common.js",
+          "@angular/animations": "/base/system/es2022/ivy/angular-animations.min.js"
         }
       }`,
   })
